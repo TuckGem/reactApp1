741de809a83551659289158e795fc1c4bc234b91
thandiwemasuku <50559699+thandiwemasuku@users.noreply.github.com>
2023-12-30 08:41:38 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import Weather from "./Weather";
+
+import "./Weather.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <Weather />
+    </div>
+    <a
+            href="https://github.com/thandiweportia"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+  </StrictMode>,
+);
